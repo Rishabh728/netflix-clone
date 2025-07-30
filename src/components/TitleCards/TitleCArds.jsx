4481c05c2ef8231d71856d6d7d2793ea68386801
Rishabh728 <trishabh728@gmail.com>
2025-07-30 10:58:31 +0530

    created multiple components & navbar

---
+++ src/components/TitleCards/TitleCArds.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TitleCArds = () => {
+  return (
+      <div className='titlecards'>
+          
+    </div>
+  )
+}
+
+export default TitleCArds
